092c020601dc25ff9233b2ea4f34378232966acb
Sander Cokart <cokart32@gmail.com>
2021-08-05 15:39:41 +0200

    added rewrites for routes and env variable for API_URL

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites () {
+    return [
+      {
+        source: '/',
+        destination: '/blog/recent',
+        permanent: true
+      }
+    ];
+  },
+  env: {
+    API_URL: 'api.localhost:8000'
+  }
+};
