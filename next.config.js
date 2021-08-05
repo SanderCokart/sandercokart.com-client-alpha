6454fcba185f73de50d442e8dc8e37dbc96e3f05
Sander Cokart <cokart32@gmail.com>
2021-08-05 16:32:58 +0200

    removed Home.module.css, updated some config files, added sass compiler and eslint rule for sorting html attributes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
+const path = require('path');
+
 module.exports = {
   reactStrictMode: true,
   async rewrites () {
     return [
       {
         source: '/',
-        destination: '/blog/recent',
-        permanent: true
+        destination: '/blog/recent'
       }
     ];
   },
   env: {
     API_URL: 'api.localhost:8000'
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
   }
 };
