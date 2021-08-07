516ddef9c9d367933e88f54465e5fca6905df254
Sander Cokart <cokart32@gmail.com>
2021-08-07 20:43:07 +0200

    added login logic and state management

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,24 +1,60 @@
+import {LoginCredentials, useAuth} from '@/providers/AuthProvider';
 import styles from '@/styles/Login.module.scss';
 import Link from 'next/link';
-import {FC} from 'react';
+import {useRouter} from 'next/router';
+import {ChangeEvent, FC, FormEvent, useState} from 'react';
 
 export const Login: FC = () => {
+  const [credentials, setCredentials] = useState<LoginCredentials>({
+    email: '',
+    password: '',
+    remember_me: false
+  });
+
+  const router = useRouter();
+  const {login} = useAuth();
+  const {query} = router;
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCredentials((prevState) => {
+      return ({
+        ...prevState,
+        [e.target.name]: e.target.value
+      });
+    });
+  };
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    login(credentials).then(({status}) => {
+      if (status === 200) {
+        if (query?.type === 'verify_email')
+          return router.push({pathname: '/verify-email', query});
+        return router.push('/dashboard');
+      }
+    });
+  };
+
+
   return (
       <div className={styles.login}>
-        <form noValidate className={styles.form}>
+        <form noValidate className={styles.form} onSubmit={onSubmit}>
 
           <div className={styles.inputs}>
             <div className={styles.formControl}>
               <label htmlFor="email">Email</label>
-              <input id="email" type="email"/>
+              <input id="email" type="email" onChange={onChange}/>
             </div>
             <div className={styles.formControl}>
               <label htmlFor="password">Password</label>
-              <input id="password" type="password"/>
+              <input id="password" type="password" onChange={onChange}/>
             </div>
             <div className={styles.remember}>
               <label htmlFor="remember_me">Remember me</label>
-              <input id="remember_me" type="checkbox"/>
+              <input id="remember_me" type="checkbox" onChange={onChange}/>
+            </div>
+            <div>
+              <button type="submit">Submit</button>
             </div>
           </div>
 
