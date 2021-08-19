858d4bf3f5c48738930a4b6b1c04726cf2959a55
Sander Cokart <cokart32@gmail.com>
2021-08-20 00:18:51 +0200

    added redirects

--- next.config.js
+++ next.config.js
@@ -2,13 +2,14 @@ const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
-  async rewrites () {
+  async redirects() {
     return [
       {
         source: '/',
-        destination: '/blog/recent'
-      }
-    ];
+        destination: '/blog/recent',
+        permanent: true,
+      },
+    ]
   },
   env: {
     API_URL: 'http://localhost:8000/',
