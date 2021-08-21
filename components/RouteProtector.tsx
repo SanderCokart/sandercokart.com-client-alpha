c10e208b3bd9630b16178fb16eb9dee449b3e2d2
Sander Cokart <cokart32@gmail.com>
2021-08-21 02:10:47 +0200

    fixed RouteProtector.tsx

--- components/RouteProtector.tsx
+++ components/RouteProtector.tsx
@@ -1,26 +1,29 @@
 import Loader from '@/components/Loader';
-import { useAuth } from '@/providers/AuthProvider';
-import { useRouter } from 'next/router';
-import type { FC } from 'react';
+import {useAuth} from '@/providers/AuthProvider';
+import {useRouter} from 'next/router';
+import type {FC} from 'react';
 
-export const RouteProtector: FC = ({ children }) => {
-  const { loggedIn, loading } = useAuth();
+export const RouteProtector: FC = ({children}) => {
+  const {loggedIn, loading} = useAuth();
   const router = useRouter();
 
-  const publicRoutes = ['/login', '/account/create', '/forgot-password', '/reset-password', '/404'];
+  const redirectIfLoggedIn = ['/login', '/account/create', '/account/password/forgot', '/account/password/reset'];
+  const requireAuth = ['/account'];
 
   if (loading) {
-    return <Loader />;
+    return <Loader/>;
   }
 
-  if (!publicRoutes.includes(router.route) && !loggedIn && !loading) {
+  /* if logged in redirect to account page - TODO send to individual corresponding routes */
+  if (redirectIfLoggedIn.includes(router.route) && loggedIn && !loading) {
+    router.push('/account');
+    return <Loader/>;
+  }
+
+  /* if page requires auth redirect to login */
+  if (requireAuth.includes(router.route) && !loggedIn && !loading) {
     router.push('/login');
-    return <Loader />;
-  }
-
-  if (publicRoutes.includes(router.route) && loggedIn && !loading) {
-    router.push('/blog/recent');
-    return <Loader />;
+    return <Loader/>;
   }
 
   return <>{children}</>;
