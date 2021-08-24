64c6bea84f2ed60235f96a7afba94dfcbed50e0d
Sander Cokart <cokart32@gmail.com>
2021-08-24 15:02:30 +0200

    added email and password request buttons to the Account page

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -2,11 +2,21 @@ import {useAuth} from '@/providers/AuthProvider';
 import type {FC} from 'react';
 
 export const Account: FC = () => {
-  const {logout} = useAuth();
+  const {logout, requestPasswordReset, requestEmailChange} = useAuth();
+
+  const handleRequestPasswordReset = () => {
+    requestPasswordReset();
+  };
+
+  const handleRequestEmailChange = () => {
+    requestEmailChange();
+  };
 
   return (
       <div>
         <button onClick={logout}>Logout</button>
+        <button onClick={handleRequestPasswordReset}>Request Password Reset</button>
+        <button onClick={handleRequestEmailChange}>Request Email Change</button>
       </div>
   );
 };
